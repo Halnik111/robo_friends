a90f372cce7f702aa7173e4975b364a5ddaf7277
Nikolas <halonikolas@gmail.com>
2022-07-28 18:55:33 +0200

    List of robots added.

---
+++ src/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({robots}) => {
+    const cardComponent = robots.map((user) => {
+        return <Card key={user.id}
+                     id={user.id}
+                     name={user.name}
+                     email={user.email}/>
+    });
+    return (
+        <div className="tc">
+            {cardComponent};
+        </div>
+    );
+}
+
+export default CardList;
